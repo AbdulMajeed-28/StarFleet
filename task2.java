77b593c48957a1629f2380fbfd76cf3c7397d34e
AbdulMajeed-28 <160846335+AbdulMajeed-28@users.noreply.github.com>
2024-04-01 01:24:07 +0530

    Update task2.java

--- task2.java
+++ task2.java
@@ -7,16 +7,16 @@ public class Main
 {
     public static void visa(String cardNumber)
     {
-        if (cardNumber.startsWith("4") && (cardNumber.length() == 12 || cardNumber.length() == 15))
+        if(cardNumber.startsWith("4") && (cardNumber.length() == 12 || cardNumber.length() == 15))
         {
             System.out.println("Visa card is valid");
 
         }
     }
     public static void amex(String cardNumber) {
-        if (cardNumber.startsWith("34") || cardNumber.startsWith("37"))
+        if(cardNumber.startsWith("34") || cardNumber.startsWith("37"))
         {
-            if (cardNumber.length() == 14)
+            if(cardNumber.length() == 14)
             {
                 System.out.println("American Express card is valid");
             }
@@ -24,9 +24,9 @@ public class Main
     }
     public static void mastercard(String cardNumber)
     {
-        if (cardNumber.startsWith("51") || cardNumber.startsWith("52") || cardNumber.startsWith("53") || cardNumber.startsWith("54") || cardNumber.startsWith("55"))
+        if(cardNumber.startsWith("51") || cardNumber.startsWith("52") || cardNumber.startsWith("53") || cardNumber.startsWith("54") || cardNumber.startsWith("55"))
         {
-            if (cardNumber.length() == 15)
+            if(cardNumber.length() == 15)
             {
                 System.out.println("Mastercard is valid");
             }
@@ -48,18 +48,18 @@ public class Main
         char lastChar = cardNumber.charAt(cardNumber.length() - 1);
         cardNumber = cardNumber.substring(0, cardNumber.length() - 1);
         List<Integer> cardDigits = new ArrayList<>();
-        for (char c : cardNumber.toCharArray())
+        for(char c : cardNumber.toCharArray())
         {
             cardDigits.add(Character.getNumericValue(c));
         }
         Collections.reverse(cardDigits);
         List<Integer> operatedDigits = new ArrayList<>();
-        for (int i = 0; i < cardDigits.size(); i++) {
+        for(int i = 0; i < cardDigits.size(); i++) {
             int digit = cardDigits.get(i);
             if (i % 2 == 0)
             {
                 int twoDigit = digit * 2;
-                if (twoDigit > 9)
+                if(twoDigit > 9)
                 {
                     twoDigit -= 9;
                 }
@@ -71,7 +71,7 @@ public class Main
             }
         }
         int total = Character.getNumericValue(lastChar) + operatedDigits.stream().mapToInt(Integer::intValue).sum();
-        if (total % 10 == 0)
+        if(total % 10 == 0)
         {
             visa(cardNumber);
             amex(cardNumber);
